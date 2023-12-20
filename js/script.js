e8f024779db63d69a1ca36435b77074bd0cfcd82
giamaf <gianluca.maffucci@hotmail.com>
2023-12-20 16:22:15 +0100

    added html's layout + js analysis

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-// Verifica collegamento JS
-console.log('JS OK');
+// STEPS
+
+// 1 - recupero gli elementi dalla pagina/DOM e preparo le variabili
+// 2 - aggancio il tasto "Conferma" all'event listener e all'interno dell'evento 
+
+//! EVENTI AL CLICK
+//? 1 - leggo i valori del form
+//? 2 - effettuo la validazione dei valori inseriti dall'utente
+//? 3 - calcolo il prezzo base e gli eventuali sconti
+//? 4 - arrotondo il prezzo
+//? 5 - svuoto i campi
+//? 6 - stampo i valori sul biglietto
+//? 7 - rendo visibile il biglietto
